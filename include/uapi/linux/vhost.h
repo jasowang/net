c71c19650c14dd29b1b7f10d1bd78d2b03a8ad8f
Jason Wang <jasowang@redhat.com>
2014-11-09 20:22:08 +0800

    vhost_net: interrupt coalescing support
    This patch implements interrupt coalescing support for vhost_net. And provides
    ioctl()s for userspace to get and set coalescing parameters. Two kinds of
    parameters were allowed to be set:
    
    - max_coalesced_frames: which is the maximum numbers of packets were allowed
      before issuing an irq.
    - coalesced_usecs: which is the maximum number of micro seconds were allowed
      before issuing an irq if at least one packet were pending.
    
    A per virtqueue hrtimer were used for coalesced_usecs.
    
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/uapi/linux/vhost.h
+++ include/uapi/linux/vhost.h
@@ -27,10 +27,17 @@ struct vhost_vring_file {
 
 };
 
+<<<<<<< current
 struct vhost_vring_coalesce {
 	unsigned int index;
 	__u32 coalesce_usecs;
 	__u32 max_coalesced_buffers;
+=======
+struct vhost_net_vring_coalesce {
+	unsigned int index;
+	__u32 coalesce_usecs;
+	__u32 max_coalesced_frames;
+>>>>>>> patched
 };
 
 struct vhost_vring_addr {
@@ -132,6 +139,12 @@ struct vhost_memory {
  * device.  This can be used to stop the ring (e.g. for migration). */
 #define VHOST_NET_SET_BACKEND _IOW(VHOST_VIRTIO, 0x30, struct vhost_vring_file)
 
+/* Setting interrupt coalescing parameters. */
+#define VHOST_NET_SET_VRING_COALESCE \
+	_IOW(VHOST_VIRTIO, 0x31, struct vhost_net_vring_coalesce)
+/* Getting interrupt coalescing parameters. */
+#define VHOST_NET_GET_VRING_COALESCE \
+	_IOW(VHOST_VIRTIO, 0x32, struct vhost_net_vring_coalesce)
 /* Feature bits */
 /* Log all write descriptors. Can be changed while device is active. */
 #define VHOST_F_LOG_ALL 26
