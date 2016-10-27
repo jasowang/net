2e9c0d0643d37b1ea418ee3e938f4c532f63dbc1
Jason Wang <jasowang@redhat.com>
2016-10-27 15:41:11 +0800

    Refresh of tuntap-convert-to-napi

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -132,6 +132,7 @@ struct vhost_virtqueue {
 	/* Log write descriptors */
 	void __user *log_base;
 	struct vhost_log *log;
+	u16 delayed;
 
 	/* Ring endianness. Defaults to legacy native endianness.
 	 * Set to true when starting a modern virtio device. */
