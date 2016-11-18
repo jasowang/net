766af317b6bbfd1aa44daaf1037c275a866a77f0
Jason Wang <jasowang@redhat.com>
2016-11-18 16:52:23 +0800

    Refresh of tuntap-rx-batching

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1347,8 +1347,11 @@ static ssize_t tun_get_user(struct tun_struct *tun, struct tun_file *tfile,
 	rxhash = skb_get_hash(skb);
 	skb_queue_tail(&tfile->socket.sk->sk_write_queue, skb);
 
-	if (!more)
+	if (!more) {
+		local_bh_disable();
 		napi_schedule(&tfile->napi);
+		local_bh_enable();
+	}
 
 	stats = get_cpu_ptr(tun->pcpu_stats);
 	u64_stats_update_begin(&stats->syncp);
