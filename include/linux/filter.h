335e664a0127307411d93d98c0c17672126f4fb1
Jason Wang <jasowang@redhat.com>
2016-12-16 15:27:59 +0800

    XDP: introduce xdp free func
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -435,7 +435,22 @@ struct bpf_skb_data_end {
 struct xdp_buff {
 	void *data;
 	void *data_end;
+	void *ring;
+	void *private;
+	void (*free) (const void *ring, const void *private);
 };
+
+static inline void xdp_buff_init(struct xdp_buff *xdp_buff,
+                                 void *data, void *data_end,
+                                 void *ring, void *private,
+                                 void *free)
+{
+	xdp_buff->data = data;
+	xdp_buff->data_end = data_end;
+	xdp_buff->ring = ring;
+	xdp_buff->private = private;
+	xdp_buff->free = free;
+}
 
 /* compute the linear packet data range [data, data_end) which
  * will be accessed by cls_bpf and act_bpf programs
