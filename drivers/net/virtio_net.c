608790dae75129727c7a4398844e34f5dfaed74d
Jason Wang <jasowang@redhat.com>
2017-03-15 12:03:35 +0800

    Refresh of virtio-net

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -356,10 +356,9 @@ static void virtnet_rx_recycle(struct receive_queue *rq, struct page *page)
 {
 	struct virtnet_page_cache *cache = &rq->page_cache;
 
-	if (page_ref_count(page) == 1 && cache->index < VIRTNET_CACHE_SIZE)
+	if (page_ref_count(page) == 2 && cache->index < VIRTNET_CACHE_SIZE)
 		cache->pages[cache->index++] = page;
-	else
-		put_page(page);
+	put_page(page);
 }
 
 static bool virtnet_xdp_xmit(struct virtnet_info *vi,
