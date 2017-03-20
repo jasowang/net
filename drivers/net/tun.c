73a5c0d7d70c468f918fd7afb9c1c55dd3bf548c
Jason Wang <jasowang@redhat.com>
2017-03-20 19:42:38 +0800

    Refresh of tun-introduce-0

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -2615,14 +2615,14 @@ EXPORT_SYMBOL_GPL(tun_get_socket);
 
 struct skb_array *tun_get_skb_array(struct file *file)
 {
-       struct tun_file *tfile;
+	struct tun_file *tfile;
 
-       if (file->f_op != &tun_fops)
-               return ERR_PTR(-EINVAL);
-       tfile = file->private_data;
-       if (!tfile)
-               return ERR_PTR(-EBADFD);
-       return &tfile->tx_array;
+	if (file->f_op != &tun_fops)
+		return ERR_PTR(-EINVAL);
+	tfile = file->private_data;
+	if (!tfile)
+		return ERR_PTR(-EBADFD);
+	return &tfile->tx_array;
 }
 EXPORT_SYMBOL_GPL(tun_get_skb_array);
 
