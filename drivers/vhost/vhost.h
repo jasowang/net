e3a529f9d2a3ac66dc7ad7e57d88d1c72adb3ed5
Jason Wang <jasowang@redhat.com>
2017-03-23 17:10:24 +0800

    vhost: accelerate vq metadata access with vmap()
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -93,6 +93,7 @@ struct vhost_virtqueue {
 	struct vring_desc __user *desc;
 	struct vring_avail __user *avail;
 	struct vring_used __user *used;
+	struct vring_used *used_addr;
 	const struct vhost_umem_node *meta_iotlb[VHOST_NUM_ADDRS];
 	struct file *kick;
 	struct file *call;
