905e1576e4fe27bbface566f3a1696ab37950214
Jason Wang <jasowang@redhat.com>
2017-07-28 11:46:58 +0800

    Refresh of tun-xdp-support

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1027,15 +1027,9 @@ static int tun_xdp_set(struct net_device *dev, struct bpf_prog *prog,
 	 */
 
 	old_prog = rtnl_dereference(tun->xdp_prog);
+	rcu_assign_pointer(tun->xdp_prog, prog);
 	if (old_prog)
 		bpf_prog_put(old_prog);
-	rcu_assign_pointer(tun->xdp_prog, prog);
-
-	if (prog) {
-		prog = bpf_prog_add(prog, 1);
-		if (IS_ERR(prog))
-			return PTR_ERR(prog);
-	}
 
 	return 0;
 }
