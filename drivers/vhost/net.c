8622d002986960ed163ebd694637a3705eafd7da
Jason Wang <jasowang@redhat.com>
2017-09-01 17:57:14 +0800

    Refresh of vhost-fix-busy-polling-in-the

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -426,7 +426,6 @@ static bool vhost_net_tx_avail(struct vhost_net *net,
 		cpu_relax();
 	preempt_enable();
 
-	printk("vq avail %d\n", vhost_vq_avail_empty(vq->dev, vq));
 	return !vhost_vq_avail_empty(vq->dev, vq);
 }
 
@@ -655,7 +654,7 @@ static int vhost_net_rx_peek_head_len(struct vhost_net *net, struct sock *sk)
 
 		preempt_enable();
 
-		if (vhost_enable_notify(&net->dev, vq))
+		if (!vhost_vq_avail_empty(&net->dev, vq))
 			vhost_poll_queue(&vq->poll);
 		mutex_unlock(&vq->mutex);
 
