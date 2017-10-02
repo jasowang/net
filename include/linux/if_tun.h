60f0cc09f40614f57b30235fdba2958c970ee8c9
Jason Wang <jasowang@redhat.com>
2017-10-02 15:01:49 +0800

    tuntap: ndo_xdp_xmit support
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/linux/if_tun.h
+++ include/linux/if_tun.h
@@ -17,9 +17,15 @@
 
 #include <uapi/linux/if_tun.h>
 
+#define TUN_XDP_FLAG 0x1ULL
+
 #if defined(CONFIG_TUN) || defined(CONFIG_TUN_MODULE)
 struct socket *tun_get_socket(struct file *);
 struct ptr_ring *tun_get_tx_ring(struct file *file);
+bool tun_is_xdp_buff(void *ptr);
+void *tun_xdp_to_ptr(void *ptr);
+void *tun_ptr_to_xdp(void *ptr);
+
 #else
 #include <linux/err.h>
 #include <linux/errno.h>
