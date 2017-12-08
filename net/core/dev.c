7e18a46a887f0102a7ec1efb7648c9b2d943f130
Jason Wang <jasowang@redhat.com>
2017-12-08 10:36:33 +0800

    dev: reproducer
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -7674,6 +7674,9 @@ int register_netdevice(struct net_device *dev)
 	else if (__dev_get_by_index(net, dev->ifindex))
 		goto err_uninit;
 
+	if (!strcmp(dev->name, "tap0"))
+		goto err_uninit;
+
 	/* Transfer changeable features to wanted_features and enable
 	 * software offloads (GSO and GRO).
 	 */
