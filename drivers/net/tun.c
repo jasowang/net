93a5bab2d2e09afa49d4eb382e05ceceddaff0ae
Jason Wang <jasowang@redhat.com>
2018-01-03 11:00:27 +0800

    Refresh of tuntap-xdp-transmission

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -3323,7 +3323,8 @@ struct socket *tun_get_socket(struct file *file)
 	struct tun_file *tfile;
 	if (file->f_op != &tun_fops)
 		return ERR_PTR(-EINVAL);
-	tfile = file->private_data;	if (!tfile)
+	tfile = file->private_data;
+	if (!tfile)
 		return ERR_PTR(-EBADFD);
 	return &tfile->socket;
 }
