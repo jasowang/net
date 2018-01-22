f1fb6a6ff0cf5eb0e0ba6a03378dcf26f952fee7
Jason Wang <jasowang@redhat.com>
2018-01-22 18:45:23 +0800

    vhost: introduce helper to prefetch desc index
    This patch introduces vhost_prefetch_desc_indices() which could batch
    fetching descriptor indices and updating used ring. copy_to_user was
    used in order to benefit from modern cpus that support fast string
    copy. Batched vring processing will be the first user.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- drivers/vhost/vhost.h
+++ drivers/vhost/vhost.h
@@ -84,6 +84,13 @@ enum vhost_uaddr_type {
 	VHOST_NUM_ADDRS = 3,
 };
 
+struct vhost_descs {
+	struct vring_desc descs[64];
+	__virtio32 indices[64];
+	int head;
+	int tail;
+};
+
 /* The virtqueue structure describes a queue attached to a device. */
 struct vhost_virtqueue {
 	struct vhost_dev *dev;
@@ -150,6 +157,8 @@ struct vhost_virtqueue {
 	bool user_be;
 #endif
 	u32 busyloop_timeout;
+
+	struct vhost_descs descs;
 };
 
 struct vhost_msg_node {
