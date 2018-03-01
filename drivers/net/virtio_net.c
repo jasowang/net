cd96f3e715483a4ff511992a92b002db4a658208
Jason Wang <jasowang@redhat.com>
2018-03-01 10:40:33 +0800

    Refresh of virtio-net-re-enable

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -1033,11 +1033,10 @@ static unsigned int get_mergeable_buf_len(struct receive_queue *rq,
 	unsigned int len;
 
 	if (room)
-		len = PAGE_SIZE - room;
-	else
-		len = hdr_len +
-		      clamp_t(unsigned int, ewma_pkt_len_read(avg_pkt_len),
-			      rq->min_buf_len, PAGE_SIZE - hdr_len);
+		return PAGE_SIZE - room;
+
+	len = hdr_len +	clamp_t(unsigned int, ewma_pkt_len_read(avg_pkt_len),
+				rq->min_buf_len, PAGE_SIZE - hdr_len);
 
 	return ALIGN(len, L1_CACHE_BYTES);
 }
